ffdfc4e15847180285ac91ecb8c8cb6c6ec4d819
egorkaw2w <85741587+egorkaw2w@users.noreply.github.com>
2024-03-15 16:58:12 +0300

    Add files via upload

---
+++ Praktika1-Entity/Bd2.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Praktika1_Entity
+{
+    /// <summary>
+    /// Логика взаимодействия для Bd2.xaml
+    /// </summary>
+    public partial class Bd2 : Window
+    {
+        private CompClubEntities Bd = new CompClubEntities();
+        public Bd2()
+        {
+            InitializeComponent();
+            Data.ItemsSource = Bd.Post.ToList();
+        }
+
+        private void Back_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow window = new MainWindow();
+            window.Show();
+            Close();
+        }
+    }
+}
